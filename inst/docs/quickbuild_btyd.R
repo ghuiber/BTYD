9693655d5af2384721b210fa657c45a1816b4980
Gabi Huiber <ghuiber@gmail.com>
2020-11-09 21:45:23 -0800

    One more commit. Cleaner quickbuild_btyd.R script.

--- inst/docs/quickbuild_btyd.R
+++ inst/docs/quickbuild_btyd.R
@@ -1,53 +1,12 @@
 # Modeled after newbuild.R
 # Run this from the command line with Rscript:
 # Rscript quickbuild_btyd.R
-library("purrr")
 library("devtools")
 
-setwd(file.path(here::here(), 'BTYD'))
+setwd(file.path(Sys.getenv()['HOME'], 'BTYD'))
 
 document()
 build()
 install()
-
-# some cleanup
-sourcepath <- file.path(here::here())
-system(paste('rm', 
-             file.path(sourcepath, 
-                       '.DS_Store')))
-system(paste('rm', 
-             file.path(sourcepath, 
-                       'inst', 
-                       '.DS_Store')))
-system(paste('rm', 
-             file.path(sourcepath, 
-                       'R', 
-                       '.DS_Store')))
-system(paste('rm', 
-             file.path(sourcepath, 
-                       'data', 
-                       '.DS_Store')))
-system(paste('rm', 
-             file.path(sourcepath, 
-                       'man', 
-                       '.DS_Store')))
-system(paste('rm', 
-             file.path(sourcepath, 
-                       'inst', 
-                       'docs', 
-                       '.DS_Store')))
-system(paste('rm', 
-             file.path(sourcepath, 
-                       'vignettes', 
-                       '.DS_Store')))
-system(paste('rm', 
-             file.path(sourcepath, 
-                       'vignettes', 'BTYD-walkthrough.log')))
-
-message('Restoring inst/doc because check() expects it')
-dir.create(file.path('inst', 'doc'))
-system(paste('cp -R',
-             'vignettes/',
-             file.path('inst', 'doc')))
-
+build_vignettes()
 check()
